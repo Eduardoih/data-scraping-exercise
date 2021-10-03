6f3e039e5d9a8588ee21fc42bab2c06dc150cc44
Eduardo <eduardopythontests@gmail.com>
2021-10-03 15:32:14 -0300

    Initial

--- main.py
+++ main.py
@@ -1,16 +1,30 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from bs4 import BeautifulSoup
+import requests
+import csv
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+page = requests.get('https://www.payscale.com/college-salary-report/majors-that-pay-you-back/bachelors')
+soup = BeautifulSoup(page.text, 'html.parser')
+all_data_table = soup.find_all(class_='data-table__row')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+new_row = []
+for row in all_data_table:
+    new_column = []
+    for column in row:
+        data = column.find(class_='data-table__value').get_text()
+        if '$' in data:
+            remove_dollar = data.replace("$", "")
+            formatted_data = remove_dollar.replace(",", ".")
+            data = float(formatted_data)
+        elif '%' in data:
+            remove_percent = data.replace("%", "")
+            data = float(remove_percent)
+        new_column.append(data)
+    new_row.append(new_column)
+
+header = ['Major', 'Early Career Pay', 'Mid-Career Pay', '% High Meaning']
+with open('payscale_data.csv', 'w', encoding='UTF8') as f:
+    writer = csv.writer(f)
+    writer.writerow(header)
+    writer.writerows(new_row)
